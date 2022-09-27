60eb319e07b310a12fa94413710f4ca5f2b2f6c3
Yukics <alejomorell@gmail.com>
2022-09-27 20:51:25 +0200

    Doned

--- Dockerfile
+++ Dockerfile
@@ -3,12 +3,25 @@ FROM rust:1.64
 # Rus app compiling
 WORKDIR /usr/src/myapp
 COPY . .
-# RUN cargo build
+RUN cargo build
 RUN cargo install --path .
 
 # Add docker-compose-wait tool -------------------
 ENV WAIT_VERSION 2.9.0
 ADD https://github.com/ufoscout/docker-compose-wait/releases/download/$WAIT_VERSION/wait /wait
 RUN chmod +x /wait
+
+ENV USER=weeb
+ENV UID=10001
+
+RUN adduser \
+    --disabled-password \
+    --gecos "" \
+    --home "/nonexistent" \
+    --shell "/sbin/nologin" \
+    --no-create-home \
+    --uid "${UID}" \
+    "${USER}"
+USER weeb:weeb
 
 CMD ["animeflv-discord"]
--- src/main.rs
+++ src/main.rs
@@ -2,36 +2,36 @@ use reqwest;
 use tokio;
 use scraper;
 use serde_json;
+use serde_json::json;
+use unicode_segmentation::UnicodeSegmentation;
+use time::Duration;
 use std::{thread, time, env};
-use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize)]
-struct Payload {
-    cmd: String,
-    url: String,
-    maxTimeout: i32
-}
-
+// Async manager/helper
 #[tokio::main]
 async fn main ()-> Result<(), Box<dyn std::error::Error>> {
 
-    // let proxy = env::var("PROXY").unwrap().to_string();
-    let proxy = "http://localhost:8191/v1";
+    // ? Get constants and parse env vars
+    let proxy = env::var("PROXY").unwrap().to_string();
+    // let proxy = "http://localhost:8191/v1";
 
-    // let animeflv_url = env::var("ANIMEFLV_URL").unwrap().to_string();
-    let animeflv_url = "https://www3.animeflv.net";
+    let animeflv_url = env::var("ANIMEFLV_URL").unwrap().to_string();
+    // let animeflv_url = "https://www3.animeflv.net";
 
-    let payload = Payload {
-        cmd: "request.get".into(),
-        url: animeflv_url.into(),
-        maxTimeout: 60000.into()
-    };
+    let millis = parse_time(env::var("CHECK_INTERVAL").unwrap().to_string());
+
+    let payload = json!({
+        "cmd": "request.get",
+        "url": &animeflv_url,
+        "maxTimeout": 60000
+    });
+
+    let mut last_anime_list_array: Vec<String> = Vec::new();
 
     loop {
-
-        // ? We create the werequest through flareresolver, cloudflare bypasser
+        // ? We create the webrequest through flareresolver, cloudflare bypasser
         let resp = reqwest::Client::new()
-            .post(proxy)
+            .post(&proxy)
             .header("ContentType", "application/json")
             .json(&payload)
             .send()
@@ -45,13 +45,14 @@ async fn main ()-> Result<(), Box<dyn std::error::Error>> {
         // ? then parses the html string to scrap it Ex: Element(<strong class="Title">), 
         let document = scraper::Html::parse_document(&html.replace("\\", ""));
         
-        let mut anime_list_array = Vec::new();
+        let mut anime_list_array: Vec<String> = Vec::new();
         let anime_list_selector = scraper::Selector::parse("main.Main ul.ListEpisodios li a").unwrap();
 
         let anime_name_selector = scraper::Selector::parse("strong.Title").unwrap();
         let anime_cap_selector = scraper::Selector::parse("span.Capi").unwrap();
         let anime_image_selector = scraper::Selector::parse("span.Image>img").unwrap();
 
+        // ? Get list of animes on landpage
         for element in document.select(&anime_list_selector) {
             let anime_link= element.value().attr("href").expect("Could not find href attribute");
             let anime_name_element = element.select(&anime_name_selector).next().expect("Could not find anime name.");
@@ -61,19 +62,101 @@ async fn main ()-> Result<(), Box<dyn std::error::Error>> {
             let anime_name = anime_name_element.text().collect::<String>();
             let anime_cap = anime_cap_element.text().collect::<String>();
 
-            println!("{:?} {:?} {:?} {:?}",animeflv_url.to_owned() + anime_link, anime_name, anime_cap, animeflv_url.to_owned() + anime_image);
-            anime_list_array.push(animeflv_url.to_owned() + anime_link);
             anime_list_array.push(anime_name);
+            anime_list_array.push(animeflv_url.to_owned() + anime_link);
             anime_list_array.push(anime_cap);
             anime_list_array.push(animeflv_url.to_owned() + anime_image);
-        } 
-        println!("{:?}", &anime_list_array[0]);
+        }   
 
-        // TODO if arrays are different get pos off the first element in old array 
-        // TODO and print only from the new untill its there
+        // ? If the animeflv landpage changes it sends as many messages as new animes have been uploaded 
+        if last_anime_list_array != anime_list_array && last_anime_list_array.len() > 0{
+            let last = &last_anime_list_array;
+            let new = &anime_list_array;
+            tokio::task::spawn(send_discord(last.to_vec(),new.to_vec()));
+        } else {
+            println!("Nothing new");
+        }
 
-        //* Sleep 1s+processing time
-        let ten_millis = time::Duration::from_millis(10000);
-        thread::sleep(ten_millis);
+        last_anime_list_array = anime_list_array;
+
+        thread::sleep(millis);
     }
+}
+
+async fn send_discord(last: Vec<String>, new: Vec<String>)-> (){
+    println!("Sending message");
+
+    let discord_wh = env::var("DISCORD_WEBHOOK").unwrap().to_string();
+    // let discord_wh = "https://discord.com/api/webhooks/1023348456426835989/ihPFQ5U6M1w-yQvG-ajsn9wxshB_gc_yqcrALxkDt9m48_r7uVquo_SFVyf0Hx9p_cOA";
+    let index = new.iter().position(|r| r == &last[0]);
+    let max_range = (index.unwrap() + 1)/4;
+    
+    for new_anime in 0..max_range{
+        
+        // ? Builds message json
+        let new_message = json!({
+            "embeds": [
+                {
+                "author": {
+                    "name": "AnimeFLV",
+                    "url": "https://www3.animeflv.net/",
+                    "icon_url": "https://descargas.ams3.digitaloceanspaces.com/images/8212/animeflv-free_android_6647_1.png"
+                },
+                "title": new[0+(new_anime*4)],
+                "url": new[1+(new_anime*4)],
+                "color": 15258703,
+                "fields": [
+                    {
+                    "name": "Cap",
+                    "value": new[2+(new_anime*4)],
+                    "inline": true
+                    }
+                ],
+                "image": {
+                        "url": new[3+(new_anime*4)]
+                    }
+                }
+            ]
+        });
+
+        // ? Sends message to discord webhook
+        let resp = reqwest::Client::new()
+        .post(&discord_wh)
+        .header("ContentType", "application/json")
+        .json(&new_message)
+        .send()
+        .await.unwrap();
+
+        // ? Checks response
+        if resp.status().is_success() {
+            println!("OK: Message sent {:?} {:?}", &new[0+(new_anime*4)], &new[2+(new_anime*4)]);
+        } else if resp.status().is_server_error() {
+            println!("ERROR: Discord server might not be available right now so the new anime notification is lost");
+        } else {
+            println!("Something else happened. Status: {:?}", resp.status());
+        }
+    }
+}
+
+// ? This function parses 10s, 5m, 1h format to miliseconds
+fn parse_time(time: String)-> Duration{
+
+    let mut milisecs: u64 = 1000;
+
+    // ? Get only numerical part, all but last character
+    let index = time.graphemes(true).count() - 1; 
+    let quantity = (&time[..index]).parse::<u64>().unwrap();
+
+    // ? Get last character
+    let format = time.chars().rev().nth(0).unwrap();
+
+    // ? Default time will be 10s
+    match format {
+        's' => milisecs = &milisecs * &quantity,
+        'm' => milisecs = &milisecs * 60 * &quantity,
+        'h' => milisecs = &milisecs * 360 * &quantity,
+         _  => milisecs = &milisecs * 10
+    }
+
+    return time::Duration::from_millis(milisecs);
 }
